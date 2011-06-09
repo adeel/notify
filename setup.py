e53cacb7c3f25f8c68ff03c1afda1a1edad0d911
Adeel Ahmad Khan <adeel@adeel.ru>
2011-06-09 18:19:09 -0400

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+  name="notify",
+  version="0.1",
+  author="Adeel Ahmad Khan",
+  author_email="adeel@adeel.ru",
+  py_modules=["notify"],
+  license="BSD")
